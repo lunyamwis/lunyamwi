a462b2f55d90507dd246c42ed95c8dd95f7ea58c
lunyamwis <lutherlunyamwi@gmail.com>
2026-02-10 13:48:45 +0300

    working on static files

--- blog/settings.py
+++ blog/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/app/static"
+
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
