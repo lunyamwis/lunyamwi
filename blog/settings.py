838f62d7858b5ba8f68120a92141f381a25ed491
lunyamwis <lutherlunyamwi@gmail.com>
2026-02-10 14:18:18 +0300

    testing

--- blog/settings.py
+++ blog/settings.py
@@ -135,11 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/app/static"
-
-STATICFILES_DIRS = [
-    "/app/static_in_env",  # (if you use it)
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 MEDIA_URL = '/media/'
