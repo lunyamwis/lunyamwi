646bcdca3befa444cb7b001d06318b419481f33f
lunyamwis <lutherlunyamwi@gmail.com>
2026-02-16 21:50:34 +0300

    testing

--- blog/settings.py
+++ blog/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',  # Use PostgreSQL engine
-#         'NAME': os.environ.get('POSTGRES_DBNAME', 'xxx').strip(),  # Database name
-#         'USER': os.environ.get('POSTGRES_USERNAME', 'xxx').strip(),  # DB user
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'xxx').strip(),  # DB password
-#         'HOST': os.environ.get('POSTGRES_HOST', 'localhost').strip(),  # DB host
-#         'PORT': os.environ.get('POSTGRES_PORT', '5432').strip(),  # DB port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Use PostgreSQL engine
+        'NAME': os.environ.get('POSTGRES_DBNAME', 'xxx').strip(),  # Database name
+        'USER': os.environ.get('POSTGRES_USERNAME', 'xxx').strip(),  # DB user
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'xxx').strip(),  # DB password
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost').strip(),  # DB host
+        'PORT': os.environ.get('POSTGRES_PORT', '5432').strip(),  # DB port
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
